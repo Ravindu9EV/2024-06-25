837d40a0a8b6ddf11baa37052cbf20f422073e1e
Ravindu9EV <yravindu18@gmail.com>
2024-06-25 22:17:02 +0530

    objects

--- app.js
+++ app.js
@@ -2,7 +2,48 @@ console.log("Hello World");
 console.log();
 let x = 4;
 console.log(x ** 2);
+
+// Assigment operations
+
+// = | += | -= | *= | /=
+
+//------------------------------
+
+// Comparison operations
+
+// == | != | >= | <= | >= | > | <
+
+// ===
+
 let isEqual = 5 == "5";
 let isThisEqual = 5 === "5";
 console.log(isEqual);
 console.log(isThisEqual);
+
+const names = "Yasantha";
+
+var age = 12;
+{
+  var a = 13;
+  //let b = 2;
+  
+}
+const c = 9
+console.log(names);
+console.log(age);
+console.log(a);
+//console.log(b);
+console.log(c);
+
+const customer = {
+    name:"saman",
+    age:20,
+    address:"matara",
+    phone:"0712349876",
+    email:"saman@.com"
+}
+
+console.log(customer)
+console.log(customer.name);
+console.log(customer.age);
+console.log(name);
