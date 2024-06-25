5da590d3b73547e1ee8c6f095bd8bc1505facf6b
Ravindu9EV <yravindu18@gmail.com>
2024-06-25 21:30:58 +0530

    comparison operator

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-console.log("test");
+console.log("Hello World");
 console.log();
+let x = 4;
+console.log(x ** 2);
+let isEqual = 5 == "5";
+let isThisEqual = 5 === "5";
+console.log(isEqual);
+console.log(isThisEqual);
